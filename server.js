c0f48df5e48c244b0cb8a95970e16ee7b712dd4f
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-01-21 22:49:53 +0530

    initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const app=express()
+
+const PORT =process.env.PORT || 3000
+const http=require("http").createServer(app)
+http.listen(PORT,()=>{
+    console.log(`Server is running at ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+//Socket connection
+const io=require('socket.io')(http)
+io.on('connection',(socket)=>{
+    console.log("Connected..")
+    socket.on('message',(msg)=>{
+      socket.broadcast.emit('message',msg)
+    })
+})
